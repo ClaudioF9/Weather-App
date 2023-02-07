bd856da508ea1d1fec451f7bf24d454f74c3fc37
ClaudioF9 <claudioferreira0284@gmail.com>
2023-02-07 23:49:15 +0000

    Created for loop for 5-day forecast and added weather icons

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 let currentContainer = $("#today")
 let forecastContainer = $("#forecast")
-let historyContainter = $("#history")
+let historyContainer = $("#history")
 //Step 1: Can you call information from the API?
 // add url and API key with ajax call
 const apiKey = "166a433c57516f51dfab1f7edaed8413";
@@ -9,6 +9,9 @@ const apiKey = "166a433c57516f51dfab1f7edaed8413";
 //This function handles event when "Search" button is click
 $("#search-button").on("click", function(event){
     event.preventDefault();
+    $(currentContainer).empty();
+    $(forecastContainer).empty();
+
     let cityName = $("#search-input").val().trim();
     //variable for API
     let queryLat = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=" + apiKey;
@@ -21,7 +24,7 @@ $("#search-button").on("click", function(event){
             console.log(geoData);
             let coordLat = geoData[0].lat;
             let coordlon = geoData[0].lon;
-            let queryURL = "https://api.openweathermap.org/data/2.5/forecast/daily?lat=" + coordLat + "&lon=" + coordlon + "&cnt=7&appid=" + apiKey;
+            let queryURL = "https://api.openweathermap.org/data/2.5/forecast/daily?lat=" + coordLat + "&lon=" + coordlon + "&cnt=6&appid=" + apiKey;
             
             
             $.ajax({
@@ -35,9 +38,9 @@ $("#search-button").on("click", function(event){
                 let dateTime = weatherData.list[0].dt * 1000
                 let dateObject = new Date(dateTime);
                 console.log(moment(dateObject).format("DD/MMM/YYYY"));
-                let currentDate = moment(dateObject).format("DD/MMM/YYYY")
+                let currentDate = moment(dateObject).format("DD/M/YYYY")
                 //-----------------------------------------------------------
-    
+                        console.log(weatherData);
                 //creating values for Main information required
                 let currentCity = $("<h2>").text(weatherData.city.name + " (" + currentDate + ")");
                 let tempTotal = weatherData.list[0].temp.min + weatherData.list[0].temp.max;
@@ -45,32 +48,41 @@ $("#search-button").on("click", function(event){
                 let currentTempShort = $("<p>").text("Temp: " + tempConvert.toFixed(2) + "℃");
                 let currentWind = $("<p>").text("Wind: " + weatherData.list[0].speed + " KPH");
                 let currentHumidity = $("<p>").text("Humidity: " + weatherData.list[0].humidity + "%");
+                let weatherIcon = weatherData.list[0].weather[0].icon;  
+                let weatherImage = $("<img>");
+                weatherImage.attr("src", "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png");
                 //appending information
+                currentCity.append(weatherImage);
                 currentContainer.append(currentCity, currentTempShort, currentWind, currentHumidity);
 
                 //creating values for 5-day forecast in a loop
 
-                let forecastDiv = $("<div>");
-                forecastContainer.append(forecastDiv);
-                forecastDiv.addClass("card col-10 col-sm-2 bg-info m-2");
+                let forecastFive = $("<h2>").text("5-day Forecast:");
+                forecastContainer.append(forecastFive);
+                forecastFive.addClass("col-12");
+                
+                for (let i = 1; i < weatherData.list.length; i++) {
+                    let forecastDiv = $("<div>");
+                    forecastContainer.append(forecastDiv);
+                    forecastDiv.addClass("card col-10 col-sm-2 bg-primary m-2");
+                  
+                    let forecastDate = $("<h4>").text(moment(new Date(weatherData.list[i].dt * 1000)).format("DD/M/YYYY"));
 
-                let forecastDate = $("<h3>").text("Date");
-                let forecastTemp = $("<p>").text("Date");
-                let forecastWind = $("<p>").text("Wind: " + weatherData.list[1].speed + " KPH");
-                let forecastHumidity = $("<p>").text("Humidity: " + weatherData.list[1].humidity + "%");
+                    let forecastIcon = weatherData.list[i].weather[0].icon;  
+                    let forecastImage = $("<img>");
+                    forecastImage.attr("src", "http://openweathermap.org/img/wn/" + forecastIcon + "@2x.png");    
 
-                forecastDiv.append(forecastDate,forecastTemp,forecastWind,forecastHumidity);
+                    let avgTemp = (weatherData.list[i].temp.min + weatherData.list[i].temp.max)/2 - "273.15";
+                    let formattedTemp = avgTemp.toFixed(2)
+                    let forecastTemp = $("<p>").text("Temp: " + formattedTemp + "℃");
+                    let forecastWind = $("<p>").text("Wind: " + weatherData.list[i].speed + " KPH");
+                    let forecastHumidity = $("<p>").text("Humidity: " + weatherData.list[i].humidity + "%");
+                  
+                    forecastDiv.append(forecastDate, forecastImage, forecastTemp, forecastWind, forecastHumidity);
+                  }
 
-                let forecastDi = $("<div>");
-                forecastContainer.append(forecastDi);
-                forecastDi.addClass("card col-10 col-sm-2 bg-info m-2");
-
-                let forecastDat = $("<h3>").text("Date");
-                let forecastTem = $("<p>").text("Date");
-                let forecastWin = $("<p>").text("Wind: " + weatherData.list[2].speed + " KPH");
-                let forecastHumidit = $("<p>").text("Humidity: " + weatherData.list[2].humidity + "%");
-
-                forecastDi.append(forecastDat,forecastTem,forecastWin,forecastHumidit);        
+                    let historyCity = $("<button>").text(weatherData.city.name);
+                    historyContainer.append(historyCity);     
 
                 }
             })
@@ -135,4 +147,4 @@ $("#search-button").on("click", function(event){
             // console.log(tempConvert);
 
             // let historyCity = $("<button>").text(response.name);
-            // historyContainter.append(historyCity);
+            // historyContainer.append(historyCity);
